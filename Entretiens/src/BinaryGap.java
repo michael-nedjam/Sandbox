bcefeb4ba9ba26112f5b5b2ecf5128e41b476826
michael <michael.nedjam@gmail.com>
2018-02-28 08:58:57 +0100

    add new unfinish test

---
+++ Entretiens/src/BinaryGap.java
@@ -0,0 +1,14 @@
+public class BinaryGap {
+
+    public static int solution(int N) {
+        // write your code in Java SE 8
+        return 0;
+    }
+
+    public static void main (String[] args) {
+        // write your code in Java SE 8
+        int result = solution(9);
+        System.out.println("Solution fond" + result);
+
+    }
+}
