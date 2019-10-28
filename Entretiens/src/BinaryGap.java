0042e484147b1a5a6874460e99154b12996ea831
michael <michael.nedjam@gmail.com>
2019-10-28 21:57:08 +0100

    Solve BinaryGap challenge

--- Entretiens/src/BinaryGap.java
+++ Entretiens/src/BinaryGap.java
@@ -1,13 +1,32 @@
 public class BinaryGap {
 
     public static int solution(int N) {
-        // write your code in Java SE 8
-        return 0;
+        int maxGap = 0;
+        int currentGap = 0;
+        boolean isGap = false;
+
+        int currentValue = N;
+        while(currentValue >= 1){
+            if(currentValue % 2 == 0){
+                if(isGap)
+                    currentGap++;
+            }else{
+                if(isGap){
+                    if(currentGap > maxGap)
+                        maxGap = currentGap;
+                    currentGap = 0;
+                }
+                isGap = true;
+                currentValue-=1;
+            }
+            currentValue/=2;
+        }
+        return maxGap;
     }
 
     public static void main (String[] args) {
         // write your code in Java SE 8
-        int result = solution(9);
+        int result = solution(1311060);
         System.out.println("Solution fond" + result);
 
     }
