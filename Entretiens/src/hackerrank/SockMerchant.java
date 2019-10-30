43b87b1ddd9d9f673cb47e7b6fe317a9e05f463d
michael <michael.nedjam@pm.me>
2019-10-30 17:22:50 +0100

    Add solution PermCheck @codility

---
+++ Entretiens/src/hackerrank/SockMerchant.java
@@ -0,0 +1,47 @@
+package hackerrank;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class SockMerchant {
+
+    // Complete the sockMerchant function below.
+    static int sockMerchant(int n, int[] ar) {
+
+        boolean[] socks = new boolean[101];
+        int nbPair = 0;
+
+        for (int i = 0; i < ar.length; i++) {
+            int color = ar[i];
+            if(socks[color])
+                nbPair++;
+            socks[color] = !socks[color];
+        }
+
+        return nbPair;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] ar = new int[n];
+
+        String[] arItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int arItem = Integer.parseInt(arItems[i]);
+            ar[i] = arItem;
+        }
+
+        int result = sockMerchant(n, ar);
+
+        System.out.println(result);
+
+        scanner.close();
+    }
+}
